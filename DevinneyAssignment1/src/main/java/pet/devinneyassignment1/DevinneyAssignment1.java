ea5cb56dd0b472f3eabff51ddaad34cbf524c6d4
ksdevinney <katie@devinney.org>
2022-06-20 18:54:33 -0500

    comments finished

--- DevinneyAssignment1/src/main/java/pet/devinneyassignment1/DevinneyAssignment1.java
+++ DevinneyAssignment1/src/main/java/pet/devinneyassignment1/DevinneyAssignment1.java
@@ -1,47 +1,67 @@
 /** 
  * This is a program that takes information through user input
- * And creates a story about a pet
+ * and creates a story about a pet
  */
 
 package pet.devinneyassignment1;
+
+// import scanner for user input
 import java.util.Scanner;
 
 /**
  *
  * @author katiedevinney
+ * @version 1.0
  */
 public class DevinneyAssignment1 {
+    
+    /**
+     * main program: no parameters needed
+     * 
+     * @param args An array of strings that contains the command line argument
+     */
 
     public static void main(String[] args) {
+        // variable for the new class
         DevinneyPet testPet;
+        
+        // variables for class parameters
         String petName;
         String petType;
         int petAge;
         int petWeight;
         String petColor;
         
+        // scanner for keyboard input
         Scanner scnr = new Scanner(System.in);
         
+        // get petName variable
         System.out.println("Enter your pet's name: ");
         petName = scnr.nextLine();
         
+        // get petType variable
         System.out.println("Enter the type of pet: ");
-        petType = scnr.nextLine();
+        petType = scnr.nextLine().toLowerCase();
         
+        // get petAge variable
         System.out.println("Enter the pet's age, in years: ");
         petAge = scnr.nextInt();
         
+        // get petWeight variable
         System.out.println("Enter the pet's weight, in pounds: ");
         petWeight = scnr.nextInt();
         
         //consume the nelwine
         scnr.nextLine();
         
+        // get petColor variable
         System.out.println("What color is your pet?");
-        petColor = scnr.nextLine();
+        petColor = scnr.nextLine().toLowerCase();
         
+        // initialize instance of DevinneyPet class
         testPet = new DevinneyPet(petName, petType, petAge, petWeight, petColor);
 
+        // print the story for the information entered
         testPet.printStory();
     }
 }
--- DevinneyAssignment1/src/main/java/pet/devinneyassignment1/DevinneyPet.java
+++ DevinneyAssignment1/src/main/java/pet/devinneyassignment1/DevinneyPet.java
@@ -1,13 +1,16 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * This is a class containing basic information about a pet
+ * It is used to create a story
  */
 package pet.devinneyassignment1;
 
 /**
  *
  * @author katiedevinney
+ * @version 1.0
  */
+
+
 public class DevinneyPet {
     private String Name;    // pet name
     private String Type;    // type of pet
@@ -23,50 +26,103 @@ public class DevinneyPet {
         Color = newColor;
     }
 
-    // class methods
+    /** 
+     * sets a name for the DevinneyPet class
+     * @param petName the name to be set
+     */
 
     public void setName(String petName) {
         this.Name = petName;
     }
 
+    /**
+     * returns the value set for Name
+     * @return String representing the name 
+     */
+    
     public String getName() {
         return Name;
     }
+    
+    /** 
+     * sets a type for the DevinneyPet class
+     * @param petType the type to be set
+     */
     
     public void setType(String petType) {
         this.Type = petType;
     }
     
+    /**
+     * returns the value set for Type
+     * @return String representing the type 
+     */
+    
     public String getType() {
         return Type;
     }
+    
+    /** 
+     * sets an age for the DevinneyPet class
+     * @param petAge the age to be set
+     */
     
     public void setAge(int petAge) {
         this.Age = petAge;
     }
     
+    /**
+     * returns the value set for Age
+     * @return int representing the age 
+     */
+    
     public int getAge() {
         return Age;
     }
+    
+    /** 
+     * sets a weight for the DevinneyPet class
+     * @param petWeight the weight to be set
+     */
 
     public void setWeight(int petWeight) {
         this.Weight = petWeight;
     }
     
+    /**
+     * returns the value set for Weight
+     * @return int representing the weight 
+     */
+    
     public int getWeight() {
         return Weight;
     }
+    
+    /** 
+     * sets a color for the DevinneyPet class
+     * @param petColor the color to be set
+     */
     
     public void setColor(String petColor) {
         this.Color = petColor;
     }
     
+    /**
+     * returns the value set for Color
+     * @return String representing the color 
+     */
+    
     public String getColor() {
         return Color;
     }
     
+    /** 
+     * Uses the object attributes to write a short story about the pet
+     */
+    
     public void printStory() {
-        System.out.printf("There once was a %s %s called %s who lived in Dallas, Texas. " + ""
+        System.out.printf("There once was a %s %s called %s who lived in Dallas, "
+                + "Texas. " + ""
                 + "In June 2022 when it was %d years old, \n%s was %d pounds." + ""
                 + " %s was a happy %s and had a long and happy life.", 
                 Color, Type, Name, Age, Name, Weight, Name, Type);
