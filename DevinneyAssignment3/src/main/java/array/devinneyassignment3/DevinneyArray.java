7cdc36c964774af86b59bd53dcf31f027219eb43
ksdevinney <katie@devinney.org>
2022-07-29 18:25:54 -0500

    starting assignment 3

---
+++ DevinneyAssignment3/src/main/java/array/devinneyassignment3/DevinneyArray.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author katiedevinney
+ */
+import java.util.ArrayList;
+
+public class DevinneyArray {
+   private ArrayList<String> Names;
+   private int Number;
+   
+   public DevinneyArray() {
+       Number = 200;
+       Names = new ArrayList<>(Number);  
+   }
+   
+//   public void Initialize() {
+//       if (!Names.isEmpty()) {
+//           Names.clear();
+//       }
+//       
+//       Number = 0;
+//       Names = new ArrayList<>(Number);
+//   }
+}
