c22757125a1229848bbefbbd17f79c7178d2ea1d
ksdevinney <katie@devinney.org>
2022-07-30 16:21:11 -0500

    most required methods

--- DevinneyAssignment3/src/main/java/array/devinneyassignment3/DevinneyArray.java
+++ DevinneyAssignment3/src/main/java/array/devinneyassignment3/DevinneyArray.java
@@ -8,6 +8,7 @@
  * @author katiedevinney
  */
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DevinneyArray {
    private ArrayList<String> Names;
@@ -26,4 +27,57 @@ public class DevinneyArray {
 //       Number = 0;
 //       Names = new ArrayList<>(Number);
 //   }
+   
+   /**
+    * reads strings from the console and adds them to the array
+    * @param N the number of elements to add
+    */
+   public void Input(int N) {
+       Scanner scnr = new Scanner(System.in);
+       int i;
+       String newName;
+       
+       for (i = 0; i < N; i++) {
+           System.out.println("Type the name you want to add: ");
+           newName = scnr.nextLine();
+           Names.add(newName);
+           Number++;
+       }
+   }
+   
+   /**
+    * Outputs the size of the array, and prints each element
+    */
+   public void Output() {
+       int i;
+       
+       System.out.printf("The array of %d items is:\n", Number);
+       
+       for (i = 0; i < Number; i++) {
+           System.out.println(Names.get(i));
+       }
+   }
+   
+   /**
+    * performs a sequential search and returns occurrences of the search term
+    * @param S search term
+    */
+   public void SearchAndOutput(String S) {
+       int i;
+       
+       for (i = 0; i < Names.size(); i++) {
+           if (Names.get(i).equals(S)) {
+               System.out.printf("%s found at index %d", S, i);
+           }
+       }
+   }
+   
+   /**
+    * returns element at specified index
+    * @param N desired index
+    * @return String element at index
+    */
+   public String Get(int N) {
+       return Names.get(N);
+   }
 }
